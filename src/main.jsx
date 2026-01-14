addce59ff2d9970a02b48ea570d214818a19bb0f
Vladislavevteev16 <Vbabangida@bk.ru>
2026-01-14 23:10:17 +0300

    fix: correct folder name typo

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
-import { ThemeProvider } from "./context/ThemeContext.jsx/index.jsx";
+import { ThemeProvider } from "./context/ThemeContext";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 
 import App from "./App.jsx";
